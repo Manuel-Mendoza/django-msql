5a830132064ff4ed2f2f4f0bbed9551597eaca55
Manuel Mendoza <55557198+Manuel-Mendoza@users.noreply.github.com>
2020-03-18 13:52:26 -0400

    yo

---
+++ BlogProject/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
